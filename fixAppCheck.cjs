db97518e6af08302e8aef29b23257fb3b93b1224
gernotfound <295053490+gernotfound@users.noreply.github.com>
2026-08-22 22:32:07 +0200

    chore: clean up temporary scripts and markdown files

--- fixAppCheck.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/lib/appCheck.ts', 'utf8');
-content = content.replace("    type AppCheckToken\n", "");
-fs.writeFileSync('src/lib/appCheck.ts', content);
--- fixCheckbox.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/components/SettingsView.tsx', 'utf8');
-
-content = content.replace(
-    /<div className="toggle-switch">[\s\S]*?<\/div>/m,
-    "<input type=\"checkbox\" id=\"analytics-toggle\" checked={analyticsEnabled} onChange={handleAnalyticsToggle} style={{ width: '24px', height: '24px', accentColor: 'var(--primary-color)' }} />"
-);
-
-fs.writeFileSync('src/components/SettingsView.tsx', content);
